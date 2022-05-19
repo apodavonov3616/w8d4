3cb8fa4aa255efdbae0783c03ecb016ece027f9e
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-19 18:22:08 -0400

    almost done with addNumbers

---
+++ intro_to_callback_exercises/addNumbers.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function addNumbers(sum, numsLeft, completionCallback) {
+    if (numsLeft > 0) {
+        reader.question("Input a number", function (incrementNumber) {
+            let num = parseInt(incrementNumber);
+
+            sum += incrementNumber
+            numsLeft -= 1
+            console.log(sum)
+
+            addNumbers(sum, numsLeft, completionCallback);
+        });
+    }
+    else {
+        completionCallback(sum)
+        reader.close()
+    }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
