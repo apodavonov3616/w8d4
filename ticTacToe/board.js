84d0ea3f2d67c95a94a3ec1d6730117c0aa7e23a
apodavonov3616 <apodavonov3616@DESKTOP-DU4IAMR.localdomain>
2022-05-19 22:18:18 -0400

    checked spot validity and placed mark

--- ticTacToe/board.js
+++ ticTacToe/board.js
@@ -11,12 +11,33 @@ class Board {
 
     }
 
+    validPos(pos) {
+        if (pos[0] >= 0 && pos[0] <= 3 && pos[1] >= 0 && pos[1] <= 3)
+            return true
+        } else {
+            return false 
+        }
+    }
+
     empty(pos) {
+        if (!Board.validPos(pos)) {
+            throw new Error('not valid');
+        }
+
+        if (this.grid[pos[0]][pos[1]] == null) {
+            return true
+        } else {
+            return false;
+        }
 
     }
 
     place_mark(pos, mark) {
+        if (!this.isEmptyPos(pos)) {
+            throw new MoveError('not available')
+        }
 
+        this.grid[pos[0]][pos[1]] = mark 
     }
 
     makeGrid() {
